5fac7539739610a121b6b48f1d59570344b3b17f
arduino12 <arad.rgb@gmail.com>
2017-07-07 09:47:03 +0300

    improve installation.sh

--- scripts/installation.sh
+++ scripts/installation.sh
@@ -1,13 +1,50 @@
+#!/usr/bin/env bash
 
-# upgrade
+### burn raspbian on SD ###
+# download raspbian-jessie-lite from https://www.raspberrypi.org/downloads/raspbian/
+# formate 4GB+ SD using infra\tools\SD-Formatter.exe
+# flash the raspbian image to SD using infra\tools\Disk-Imager.exe
+# for headless pi:
+#   add file named "ssh" to SD
+#   after power up the pi it prints its ip
+# without screen:
+#   add "enable_uart=1" to config.txt in SD
+#   connect USB-UART, PI pins:
+#   ........................................
+#   ...................................RXTXGND..(pi corner)
+#   after power up the pi, login in the usb terminal as (pi, raspberry)
+#   enter command "sudo raspi-config" -> Interfacing Options -> SSH -> YES
+#   enter command "ifconfig" to see its ip
+# now ssh to its pi@192.168.1.X
+
+### user ###
+default: pi raspberry
+# set password to "none", ";" is \n
+sudo passwd pi; none; none
+
+### info ###
+cat /proc/cpuinfo
+
+### config ###
+sudo raspi-config
+sudo echo "display_rotate=2" >> /boot/config.txt
+sudo echo "enable_uart=1" >> /boot/config.txt
+
+### upgrade ###
 sudo apt-get update && sudo apt-get upgrade -y
 sudo apt-get dist-upgrade -y
 sudo rpi-update
 
-# apps
-sudo apt-get install git samba samba-common-bin -y
+### chmodes ###
+sudo chmod +x *.sh
 
-# python3.6
+### apps ###
+sudo apt-get install git samba samba-common-bin -y
+for i in "kodi" "vlc" "tortoisehg" "curl" "openjdk-8-jre" "bluetooth" "bluez"; do
+	sudo apt-get install "$i" -y
+done
+
+### python3.6 ###
 RELEASE=3.6.1
 # install dependencies
 sudo apt-get install libbz2-dev liblzma-dev libsqlite3-dev libncurses5-dev libgdbm-dev zlib1g-dev libreadline-dev libssl-dev tk-dev -y
@@ -23,39 +60,28 @@ sudo make install
 sudo rm -rf ~/python3/Python-$RELEASE
 cd ~
 
-# python3 packages
+### python3 packages ###
 sudo pip3 install --upgrade pip
 sudo pip3 install ipython rpyc pygame Pillow
 sudo pip3 install --upgrade pyserial
+python3 -m pip install django pygame
 
-# audo serial port naming
-sudo nano /etc/udev/rules.d/90-usb-serial.rules
-SUBSYSTEM=="tty",KERNELS=="1-1.2:1.0",SYMLINK+="arduino_uv"
-SUBSYSTEM=="tty",KERNELS=="1-1.4:1.0",SYMLINK+="gsm_a6"
-
-# chmodes
-sudo chmod +x *.sh
-
-# systemd service
+### systemd service ###
 sudo nano /lib/systemd/system/uv_bicycle.service
-
 [Unit]
 Description=UV Bicycle
 After=multi-user.target
-
 [Service]
 Type=simple
 ExecStart=/bin/bash /home/pi/Public/uv_bicycle/run_gsm_to_arduino.sh --interface rpyc
 Restart=on-abort
-
 [Install]
 WantedBy=multi-user.target
-
+# after changing uv_bicycle.service run daemon-reload
 sudo chmod 644 /lib/systemd/system/uv_bicycle.service
 sudo systemctl daemon-reload
 sudo systemctl enable uv_bicycle.service
 sudo systemctl start uv_bicycle.service
-
 # Check status
 sudo systemctl status uv_bicycle.service
 # Start service
@@ -64,3 +90,86 @@ sudo systemctl start uv_bicycle.service
 sudo systemctl stop uv_bicycle.service
 # Check service's log
 sudo journalctl -f -u hello.service
+
+### bluetooth ###
+# https://www.cnet.com/how-to/how-to-setup-bluetooth-on-a-raspberry-pi-3/
+sudo bluetoothctl
+[bluetooth]# agent on
+[bluetooth]# default-agent
+[bluetooth]# scan on
+[bluetooth]# pair 98:D3:31:B2:CA:59
+[bluetooth]# trust 98:D3:31:B2:CA:59
+[bluetooth]# quit
+# create serial dev for bt connection
+sudo rfcomm connect hci0 98:D3:31:90:30:3A &
+
+### serial ports ###
+# print dev info
+udevadm info -q path -n /dev/ttyUSB1
+# auto serial port shortcuts
+sudo nano /etc/udev/rules.d/90-usb-serial.rules
+SUBSYSTEM=="tty",KERNELS=="1-1.2:1.0",SYMLINK+="arduino_uv"
+SUBSYSTEM=="tty",KERNELS=="1-1.4:1.0",SYMLINK+="gsm_a6"
+# ttyUSB
+sudo chmod 666 /dev/ttyUSB0
+lsmod | grep cp210x
+dmesg
+ls -la /dev/ttyUSB*
+sudo nano /etc/udev/rules.d/ttyUSB.rules
+KERNEL=="ttyUSB[0 … 9]" SYMLINK+="%k" GROUP="lab" MODE="0666"
+
+### miniterm ###
+python3 /usr/local/lib/python3.6/site-packages/serial/tools/miniterm.py /dev/ttyUSB1 38400 --eol CRLF
+
+### samba ###
+sudo smbpasswd -a pi; ; 
+sudo nano /etc/samba/smb.conf
+force user=pi
+workgroup = WORKGROUP
+wins support = yes
+
+[Public]
+ comment=Shared Public
+ path=/home/pi/Public
+ browseable=Yes
+ writeable=Yes
+ guest ok=yes
+ create mask=0777
+ directory mask=0777
+ public=yes
+sudo chmod a+w Public/
+sudo /etc/init.d/samba restart
+
+### language ###
+sudo setxkbmap -option grp:switch,grp:alt_shift_toggle,grp_led:scroll de,tr,us
+
+### wine ###
+ln -s /dev/ttyUSB0 ~/.wine/dosdevices/com2
+wine /home/lab/Desktop/ESP/LuaLoader/LuaLoader.exe
+
+### kodi ###
+cp <start_kodi.sh> /usr/local/bin/start_kodi.sh
+sudo chmod a+x /usr/local/bin/start_kodi.sh
+nano Desktop/KODI
+[Desktop Entry]
+Name=KODI
+Comment=KODI MEDIA CENTER
+Exec=/usr/local/bin/startkodi
+Icon=/usr/share/kodi/media/icon256x256.png
+Terminal=false
+Type=Application
+Categories=Sound;Video;
+StartupNotify=true
+
+### vlc youtube hack ###
+sudo rm /usr/lib/vlc/lua/playlist/youtube.*
+sudo curl "http://git.videolan.org/?p=vlc.git;a=blob_plain;f=share/lua/playlist/youtube.lua;hb=HEAD" -o /usr/lib/vlc/lua/playlist/youtube.lua
+http://addons.videolan.org/content/show.php/+Youtube+playlist?content=149909
+sudo mv Downloads/playlist_youtube.lua /usr/lib/vlc/lua/playlist/
+
+### links ###
+# https://mtantawy.com/quick-tip-how-to-update-to-latest-kodi-16-jarvis-on-raspberry-pi/
+# http://g8ogj.org/files/Using%20USB%20serial%20ports%20under%20wine%20howwto%20ipb.pdf
+# https://www.silabs.com/products/mcu/Pages/USBtoUARTBridgeVCPDrivers.aspx
+# https://www.silabs.com/Support%20Documents/Software/Linux_CP210x_VCP_3.x.x_Release_Notes.txt
+# http://www.oracle.com/webfolder/technetwork/tutorials/obe/java/RaspberryPiFX/raspberryfx.html
