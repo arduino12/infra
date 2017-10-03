f54d818ff115ed5a41dfa58b5e4328e3c8e26003
arduino12 <arduino12@users.noreply.github.com>
2017-10-03 22:56:07 +0300

    add gdrive binaries

--- scripts/installation.bat
+++ scripts/installation.bat
@@ -8,3 +8,9 @@ REM change PATH to point to python 3.6
 C:\Users\arad-lab\AppData\Local\Programs\Python\Python36\Scripts\;C:\Users\arad-lab\AppData\Local\Programs\Python\Python36\
 
 REM change C:\python27 to C:\_python27
+
+REM move all subfolder files to master folder
+FOR /R "." %i IN (*.*) DO MOVE "%i" "C:\Users\User\Google Drive\Arad\Pictures\Conference"
+
+REM power-shell reverse file names
+gci -File -Recurse | %{$a = $_.BaseName -split "";[array]::Reverse($a);$s=$a -join "";ren $_.FullName ($s + $_.Extension)}
