829654d72bdd374637304e57b4970b824fd1d48f
arduino12 <arad.rgb@gmail.com>
2018-06-29 06:37:01 +0300

    split app_client cli part from logic part

--- run/app_client.py
+++ run/app_client.py
@@ -8,39 +8,15 @@ from infra.core import utils
 
 class _App(object):
 
-    def __init__(self):
-        _parser = argparse.ArgumentParser(
-            description='App Client',
-            add_help=False)
-        _parser.add_argument(
-            '--rpyc_addr',
-            metavar='<ip address>',
-            dest='rpyc_addr',
-            type=str,
-            default='localhost',
-            help='the remote app rpyc address')
-        _parser.add_argument(
-            '--rpyc_port',
-            metavar='<port number>',
-            dest='rpyc_port',
-            type=int,
-            default=rpyc.utils.classic.DEFAULT_SERVER_PORT,
-            help='the remote app rpyc port')
-        _parser.add_argument(
-            '--cmd',
-            metavar='<python line>',
-            dest='cmd',
-            type=str,
-            default='',
-            help='python line insted of running ipython')
-
-        self._args, _ = _parser.parse_known_args()
+    def __init__(self, rpyc_addr=None, rpyc_port=None):
         self._conn_ = None
+        self._rpyc_addr_ = rpyc_addr or 'localhost'
+        self._rpyc_port_ = rpyc_port or rpyc.utils.classic.DEFAULT_SERVER_PORT
         self._app_attrs_ = utils.get_exposed_attrs(self)
 
     def reconnect(self):
         self.disconnect()
-        self._conn_ = rpyc.connect(self._args.rpyc_addr, self._args.rpyc_port)
+        self._conn_ = rpyc.connect(self._rpyc_addr_, self._rpyc_port_)
         self.reload(False)
 
     def disconnect(self):
@@ -58,7 +34,33 @@ class _App(object):
 
 
 if __name__ == '__main__':
-    app = _App()
+    _parser = argparse.ArgumentParser(
+        description='App Client',
+        add_help=False)
+    _parser.add_argument(
+        '--rpyc_addr',
+        metavar='<ip address>',
+        dest='rpyc_addr',
+        type=str,
+        default='localhost',
+        help='the remote app rpyc address')
+    _parser.add_argument(
+        '--rpyc_port',
+        metavar='<port number>',
+        dest='rpyc_port',
+        type=int,
+        default=rpyc.utils.classic.DEFAULT_SERVER_PORT,
+        help='the remote app rpyc port')
+    _parser.add_argument(
+        '--cmd',
+        metavar='<python line>',
+        dest='cmd',
+        type=str,
+        default='',
+        help='python line insted of running ipython')
+    _args, _ = _parser.parse_known_args()
+    app = _App(_args.rpyc_addr, _args.rpyc_port)
+    app._args = _args
     try:
         app.reconnect()
     except Exception:
